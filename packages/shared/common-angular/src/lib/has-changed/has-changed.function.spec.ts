7792db1df555f621d616228f23aaf8315d2ff842
RicardoJBarrios <RicardoJBarrios@users.noreply.github.com>
2021-06-19 23:30:56 +0100

    ci(kaikokeke): update ci
    Signed-off-by: RicardoJBarrios <RicardoJBarrios@users.noreply.github.com>

--- packages/shared/common-angular/src/lib/has-changed/has-changed.function.spec.ts
+++ packages/shared/common-angular/src/lib/has-changed/has-changed.function.spec.ts
@@ -20,4 +20,8 @@ describe('hasChanged(simpleChange)', () => {
     expect(hasChanged(new SimpleChange(a1, b, false))).toEqual(true);
     expect(hasChanged(new SimpleChange(a1, a2, false))).toEqual(false);
   });
+
+  it(`fake test`, () => {
+    expect(true).toEqual(true);
+  });
 });
