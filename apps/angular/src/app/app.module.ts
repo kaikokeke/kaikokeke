a13afac9a5bf5150d31e7935dd29bd38f0ab1e5b
RicardoJBarrios <RicardoJBarrios@users.noreply.github.com>
2021-07-20 09:45:52 +0100

    feat(apps): add Angular environment
    Signed-off-by: RicardoJBarrios <RicardoJBarrios@users.noreply.github.com>

--- apps/angular/src/app/app.module.ts
+++ apps/angular/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+import { AkitaEnvironmentModule } from './environment/environment.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, environment.production ? [] : AkitaNgDevtools.forRoot(), AkitaEnvironmentModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ apps/angular/src/app/environment/environment.module.ts
@@ -0,0 +1,116 @@
+import { APP_INITIALIZER, Inject, Injectable, InjectionToken, ModuleWithProviders, NgModule } from '@angular/core';
+import { Query, Store, StoreConfig } from '@datorama/akita';
+import {
+  EnvironmentServiceGateway,
+  EnvironmentStoreGateway,
+  LoadType,
+  Properties,
+  PropertiesSource
+} from '@kaikokeke/environment';
+import { Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
+@Injectable({ providedIn: 'root' })
+@StoreConfig({ name: 'environment', resettable: true })
+export class AkitaEnvironmentStore extends Store<Properties> {
+  constructor() {
+    super({});
+  }
+}
+
+@Injectable({ providedIn: 'root' })
+export class AkitaEnvironmentQuery extends Query<Properties> {
+  constructor(protected store: AkitaEnvironmentStore) {
+    super(store);
+  }
+}
+
+@Injectable({ providedIn: 'root' })
+export class EnvironmentStore implements EnvironmentStoreGateway {
+  constructor(protected readonly store: AkitaEnvironmentStore) {}
+
+  getProperties(): Properties {
+    return this.store.getValue();
+  }
+
+  updateProperties(newProperties: Properties): void {
+    this.store.reset();
+    this.store.update(newProperties);
+  }
+
+  resetProperties(): void {
+    this.store.reset();
+  }
+}
+
+@Injectable({ providedIn: 'root' })
+export class TestSources extends PropertiesSource {
+  readonly name = 'TestSources';
+  readonly loadType: LoadType = LoadType.DEFERRED;
+
+  load(): Observable<Properties> {
+    return of({ a: 0 }).pipe(delay(3000));
+  }
+}
+
+@Injectable({ providedIn: 'root' })
+export class TestSources2 extends PropertiesSource {
+  readonly name = 'TestSources2';
+  readonly loadType: LoadType = LoadType.DEFERRED;
+  readonly completeLoading = true;
+
+  load(): Observable<Properties> {
+    return of({ b: 0 }).pipe(delay(3000));
+  }
+}
+
+@Injectable({ providedIn: 'root' })
+export class TestSources3 extends PropertiesSource {
+  readonly name = 'TestSources3';
+  readonly loadType: LoadType = LoadType.DEFERRED;
+
+  load(): Observable<Properties> {
+    return of({ c: 0 }).pipe(delay(5000));
+  }
+}
+
+export const ENVIRONMENT_SOURCES: InjectionToken<PropertiesSource[]> = new InjectionToken<PropertiesSource[]>(
+  'ENVIRONMENT_SOURCES'
+);
+
+@Injectable({ providedIn: 'root' })
+export class EnvironmentService extends EnvironmentServiceGateway {
+  constructor(
+    protected readonly store: EnvironmentStore,
+    @Inject(ENVIRONMENT_SOURCES) protected readonly sources: PropertiesSource[]
+  ) {
+    super(store);
+  }
+}
+
+export function environmentForRoot(service: EnvironmentService) {
+  return () => service.load();
+}
+
+@NgModule({
+  providers: [
+    { provide: ENVIRONMENT_SOURCES, useClass: TestSources, multi: true },
+    { provide: ENVIRONMENT_SOURCES, useClass: TestSources2, multi: true },
+    { provide: ENVIRONMENT_SOURCES, useClass: TestSources3, multi: true },
+  ],
+})
+export class AkitaEnvironmentModule {
+  static forRoot(): ModuleWithProviders<AkitaEnvironmentModule> {
+    return {
+      ngModule: AkitaEnvironmentModule,
+      providers: [
+        {
+          provide: APP_INITIALIZER,
+          useFactory: environmentForRoot,
+          deps: [EnvironmentService],
+          multi: true,
+        },
+      ],
+    };
+  }
+}
