499ea23b7045c04d9092efaced888b766c60b595
RicardoJBarrios <RicardoJBarrios@users.noreply.github.com>
2021-07-27 22:41:44 +0100

    chore(kaikokeke): update deps
    Signed-off-by: RicardoJBarrios <RicardoJBarrios@users.noreply.github.com>

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 module.exports = {
   projects: [
+    ...getJestProjects(),
     '<rootDir>/packages/shared/devtools',
     '<rootDir>/packages/shared/common',
     '<rootDir>/packages/shared/common-angular',
