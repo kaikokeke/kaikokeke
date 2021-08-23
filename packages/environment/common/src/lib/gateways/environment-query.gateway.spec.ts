0df6a94dac0986f0ee566cfaa9ccf0b8fa334286
RicardoJBarrios <RicardoJBarrios@users.noreply.github.com>
2021-08-23 22:38:41 +0100

    feat(environment): wip Query
    Signed-off-by: RicardoJBarrios <RicardoJBarrios@users.noreply.github.com>

--- packages/environment/common/src/lib/gateways/environment-query.gateway.spec.ts
+++ packages/environment/common/src/lib/gateways/environment-query.gateway.spec.ts
@@ -25,6 +25,9 @@ class TestQuery extends EnvironmentQueryGateway {
   }
 }
 
+const environment = { a: { a: 0 } };
+const environment2 = { a: { b: 0 } };
+
 describe('EnvironmentQueryGateway', () => {
   let store: EnvironmentStoreGateway;
   let query: EnvironmentQueryGateway;
@@ -39,21 +42,111 @@ describe('EnvironmentQueryGateway', () => {
   });
 
   it(
-    `containsProperty$(path) returns if the path exists as observable`,
+    `containsProperty$(path) returns if the path exists as Observable`,
     marbles((m) => {
-      jest.spyOn(store, 'getAll$').mockReturnValue(m.cold('-a-a-b-b-a-|', { a: { a: 0 }, b: { b: 0 } }));
+      jest.spyOn(store, 'getAll$').mockReturnValue(m.cold('-a-a-b-b-a-|', { a: environment, b: environment2 }));
       const expected = m.cold('-t---f---t-|', { t: true, f: false });
-      m.expect(query.containsProperty$('a')).toBeObservable(expected);
+      m.expect(query.containsProperty$('a.a')).toBeObservable(expected);
     }),
   );
 
   it(`containsProperty(path) returns true if the path exists`, () => {
-    jest.spyOn(store, 'getAll').mockReturnValue({ a: { a: 0 } });
+    jest.spyOn(store, 'getAll').mockReturnValue(environment);
     expect(query.containsProperty('a.a')).toBeTrue();
   });
 
   it(`containsProperty(path) returns false if the path doesn't exist`, () => {
-    jest.spyOn(store, 'getAll').mockReturnValue({ a: { a: 0 } });
+    jest.spyOn(store, 'getAll').mockReturnValue(environment);
     expect(query.containsProperty('a.b')).toBeFalse();
+  });
+
+  it(
+    `getProperties$() returns the properties as Observable`,
+    marbles((m) => {
+      jest.spyOn(store, 'getAll$').mockReturnValue(m.cold('-a-a-b-b-a-|', { a: environment, b: environment2 }));
+      const expected = m.cold('-a---b---a-|', { a: environment, b: environment2 });
+      m.expect(query.getProperties$()).toBeObservable(expected);
+    }),
+  );
+
+  it(`getProperties() returns the properties`, () => {
+    jest.spyOn(store, 'getAll').mockReturnValue(environment);
+    expect(query.getProperties()).toEqual(environment);
+  });
+
+  it(
+    `getProperty$(path) returns the property value as Observable`,
+    marbles((m) => {
+      jest.spyOn(store, 'getAll$').mockReturnValue(m.cold('-a-a-b-b-a-|', { a: environment, b: environment2 }));
+      const expected = m.cold('-a---b---a-|', { a: 0, b: undefined });
+      m.expect(query.getProperty$('a.a')).toBeObservable(expected);
+    }),
+  );
+
+  it(`getProperty(path) returns the property value`, () => {
+    jest.spyOn(store, 'getAll').mockReturnValue(environment);
+    expect(query.getProperty('a.a')).toEqual(0);
+  });
+
+  it(`getProperty(path) returns undefined if no path`, () => {
+    jest.spyOn(store, 'getAll').mockReturnValue(environment);
+    expect(query.getProperty('a.b')).toBeUndefined();
+  });
+
+  it(
+    `getTypedProperty$(path, targetType) returns the typed property value as Observable`,
+    marbles((m) => {
+      jest.spyOn(store, 'getAll$').mockReturnValue(m.cold('-a-a-b-b-a-|', { a: environment, b: environment2 }));
+      const expected = m.cold('-a---b---a-|', { a: '0', b: undefined });
+      m.expect(query.getTypedProperty$('a.a', String)).toBeObservable(expected);
+    }),
+  );
+
+  it(`getTypedProperty(path, targetType) returns the typed property value`, () => {
+    jest.spyOn(store, 'getAll').mockReturnValue(environment);
+    expect(query.getTypedProperty('a.a', String)).toEqual('0');
+  });
+
+  it(`getTypedProperty(path, targetType) returns undefined if no path`, () => {
+    jest.spyOn(store, 'getAll').mockReturnValue(environment);
+    expect(query.getTypedProperty('a.b', String)).toBeUndefined();
+  });
+
+  it(
+    `getRequiredProperty$(path, defaultValue) returns the property value or default value if no path as Observable`,
+    marbles((m) => {
+      jest.spyOn(store, 'getAll$').mockReturnValue(m.cold('-a-a-b-b-a-|', { a: environment, b: environment2 }));
+      const expected = m.cold('-a---b---a-|', { a: 0, b: 1 });
+      m.expect(query.getRequiredProperty$('a.a', 1)).toBeObservable(expected);
+    }),
+  );
+
+  it(`getRequiredProperty(path, defaultValue) returns the property value`, () => {
+    jest.spyOn(store, 'getAll').mockReturnValue(environment);
+    expect(query.getRequiredProperty('a.a', 1)).toEqual(0);
+  });
+
+  it(`getRequiredProperty(path, defaultValue) returns default value if no path`, () => {
+    jest.spyOn(store, 'getAll').mockReturnValue(environment);
+    expect(query.getRequiredProperty('a.b', 1)).toEqual(1);
+  });
+
+  it(
+    `getRequiredTypedProperty$(path, defaultValue, targetType) returns the typed property value or the typed default value if no path as Observable`,
+    marbles((m) => {
+      jest.spyOn(store, 'getAll$').mockReturnValue(m.cold('-a-a-b-b-a-|', { a: environment, b: environment2 }));
+      const expected = m.cold('-a---b---a-|', { a: '0', b: '1' });
+      m.expect(query.getRequiredTypedProperty$('a.a', 1, String)).toBeObservable(expected);
+    }),
+  );
+
+  it(`getRequiredTypedProperty(path, defaultValue, targetType) returns the typed property value`, () => {
+    jest.spyOn(store, 'getAll').mockReturnValue(environment);
+    expect(query.getRequiredTypedProperty('a.a', 1, String)).toEqual('0');
+  });
+
+  it(`getRequiredTypedProperty(path, defaultValue, targetType) returns the typed default value if no path`, () => {
+    jest.spyOn(store, 'getAll').mockReturnValue(environment);
+    expect(query.getRequiredTypedProperty('a.b', 1, String)).toEqual('1');
   });
 });
--- packages/environment/common/src/lib/gateways/environment-query.gateway.ts
+++ packages/environment/common/src/lib/gateways/environment-query.gateway.ts
@@ -28,4 +28,119 @@ export abstract class EnvironmentQueryGateway {
   containsProperty(path: Path): boolean {
     return get(this.store.getAll(), path) !== undefined;
   }
+
+  /**
+   * Gets all the environment properties.
+   * @returns The environment properties values as Observable.
+   */
+  getProperties$(): Observable<Properties> {
+    return this.store.getAll$().pipe(distinctUntilChanged());
+  }
+
+  /**
+   * Gets all the environment properties.
+   * @returns The environment properties values.
+   */
+  getProperties(): Properties {
+    return this.store.getAll();
+  }
+
+  /**
+   * Gets the property value associated with the given path.
+   * @param path The property path to resolve.
+   * @returns The property value as Observable or `undefined` if the path cannot be resolved.
+   */
+  getProperty$<V>(path: Path): Observable<V | undefined> {
+    return this.store.getAll$().pipe(
+      map((properties: Properties) => get(properties, path)),
+      distinctUntilChanged(),
+    );
+  }
+
+  /**
+   * Gets the property value associated with the given path.
+   * @param path The property path to resolve.
+   * @returns The property value or `undefined` if the path cannot be resolved.
+   */
+  getProperty<V>(path: Path): V | undefined {
+    return get(this.store.getAll(), path);
+  }
+
+  /**
+   * Gets the typed property value associated with the given path.
+   * @param path The property path to resolve.
+   * @param targetType The expected type converter function.
+   * @returns The property value converted to the `targetType` as Observable
+   * or `undefined` if the path cannot be resolved.
+   */
+  getTypedProperty$<T, V>(path: Path, targetType: (value: T) => V): Observable<V | undefined> {
+    return this.store.getAll$().pipe(
+      map((properties: Properties) => get(properties, path)),
+      map((value?: T) => (value === undefined ? undefined : targetType(value))),
+      distinctUntilChanged(),
+    );
+  }
+
+  /**
+   * Gets the typed property value associated with the given path.
+   * @param path The property path to resolve.
+   * @param targetType The expected type converter function.
+   * @returns The property value converted to the `targetType` or `undefined` if the path cannot be resolved.
+   */
+  getTypedProperty<T, V>(path: Path, targetType: (value: T) => V): V | undefined {
+    const value: T = get(this.store.getAll(), path);
+
+    return value === undefined ? undefined : targetType(value);
+  }
+
+  /**
+   * Gets the required property value associated with the given path.
+   * @param path The property path to resolve.
+   * @param defaultValue The default value to return if no value is found.
+   * @returns The property value as Observable or `defaultValue` if the path cannot be resolved.
+   */
+  getRequiredProperty$<V>(path: Path, defaultValue: V): Observable<V> {
+    return this.store.getAll$().pipe(
+      map((properties: Properties) => get(properties, path, defaultValue)),
+      distinctUntilChanged(),
+    );
+  }
+
+  /**
+   * Gets the required property value associated with the given path.
+   * @param path The property path to resolve.
+   * @param defaultValue The default value to return if no value is found.
+   * @returns The property value or `defaultValue` if the path cannot be resolved.
+   */
+  getRequiredProperty<V>(path: Path, defaultValue: V): V {
+    return get(this.store.getAll(), path, defaultValue);
+  }
+
+  /**
+   * Gets the required typed property value associated with the given path.
+   * @param path The property path to resolve.
+   * @param defaultValue The default value to return if no value is found.
+   * @param targetType The expected type converter function.
+   * @returns The property value converted to the `targetType` as Observable
+   * or the converted `defaultValue` if the path cannot be resolved.
+   */
+  getRequiredTypedProperty$<T, V>(path: Path, defaultValue: T, targetType: (value: T) => V): Observable<V> {
+    return this.store.getAll$().pipe(
+      map((properties: Properties) => get(properties, path, defaultValue)),
+      map((value: T) => targetType(value)),
+      distinctUntilChanged(),
+    );
+  }
+
+  /**
+   * Gets the required typed property value associated with the given path.
+   * @param path The property path to resolve.
+   * @param defaultValue The default value to return if no value is found.
+   * @param targetType The expected type converter function.
+   * @returns The property value converted to the `targetType`
+   * or the converted `defaultValue` if the path cannot be resolved.
+   */
+  getRequiredTypedProperty<T, V>(path: Path, defaultValue: T, targetType: (value: T) => V): V {
+    return targetType(get(this.store.getAll(), path, defaultValue));
+  }
 }
