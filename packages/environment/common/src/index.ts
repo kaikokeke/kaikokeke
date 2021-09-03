dcd6dc7bab4a6a3a647d8fc8ee51123c78202d53
RicardoJBarrios <RicardoJBarrios@users.noreply.github.com>
2021-09-03 23:44:28 +0100

    refactor(environment): move gateways
    Signed-off-by: RicardoJBarrios <RicardoJBarrios@users.noreply.github.com>

--- packages/environment/common/src/index.ts
+++ packages/environment/common/src/index.ts
@@ -1,3 +1,2 @@
 export * from './lib/application/index';
-export * from './lib/gateways/index';
 export * from './lib/types/index';
--- packages/environment/common/src/lib/application/index.ts
+++ packages/environment/common/src/lib/application/index.ts
@@ -1,1 +1,6 @@
 export * from './environment-config-factory.function';
+export * from './environment-loader.gateway';
+export * from './environment-query.gateway';
+export * from './environment-service.gateway';
+export * from './environment-store.gateway';
+export * from './properties-source.gateway';
