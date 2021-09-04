f3b4478196f1344732d65b9eeff373d6e662ec3f
RicardoJBarrios <RicardoJBarrios@users.noreply.github.com>
2021-09-04 20:59:33 +0100

    feat(environment): creation of Environment Angular
    Signed-off-by: RicardoJBarrios <RicardoJBarrios@users.noreply.github.com>

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,5 @@
 const { getJestProjects } = require('@nrwl/jest');
 
 module.exports = {
-  projects: [
-    ...getJestProjects(),
-    '<rootDir>/packages/shared/devtools',
-    '<rootDir>/packages/shared/common',
-    '<rootDir>/packages/shared/common-angular',
-    '<rootDir>/packages/environment/common',
-    '<rootDir>/apps/angular',
-  ],
+  projects: [...getJestProjects()],
 };
