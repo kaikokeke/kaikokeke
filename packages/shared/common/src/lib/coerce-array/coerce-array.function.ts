25d147bfa637d7c0e262c711a05a0a32cf72b1cf
RicardoJBarrios <RicardoJBarrios@users.noreply.github.com>
2021-09-05 01:38:40 +0100

    feat(common): add coerceArray undefined arg
    Signed-off-by: RicardoJBarrios <RicardoJBarrios@users.noreply.github.com>

--- packages/shared/common/src/lib/coerce-array/coerce-array.function.ts
+++ packages/shared/common/src/lib/coerce-array/coerce-array.function.ts
@@ -1,3 +1,7 @@
-export function coerceArray<T>(value: T | T[]): T[] {
+export function coerceArray<T>(value?: T | T[]): T[] {
+  if (value == null) {
+    return [];
+  }
+
   return Array.isArray(value) ? value : [value];
 }
