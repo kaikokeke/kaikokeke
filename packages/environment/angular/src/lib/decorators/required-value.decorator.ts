57828859b856a330e8e8994e28890cc9fbdf7956
RicardoJBarrios <RicardoJBarrios@users.noreply.github.com>
2021-09-05 09:11:39 +0100

    feat(environment): add RequiredValue decorator
    Signed-off-by: RicardoJBarrios <RicardoJBarrios@users.noreply.github.com>

---
+++ packages/environment/angular/src/lib/decorators/required-value.decorator.ts
@@ -0,0 +1,36 @@
+import { serviceInjector } from '@kaikokeke/angular';
+import { EnvironmentQuery, Path, Property } from '@kaikokeke/environment';
+
+/**
+ * Gets the distinct environment property at path as mutable and sets it in the property.
+ * @param key The property path to set.
+ * @param defaultValue The value to return if the path cannot be resolved.
+ * @throws If no `ServiceInjectorModule` imported.
+ */
+export function RequiredValue$<D extends Property>(
+  key: Path,
+  defaultValue: D,
+): (target: unknown, propertyKey: string) => void {
+  return (target: unknown, propertyKey: string) => {
+    Object.defineProperty(target, propertyKey, {
+      value: serviceInjector(EnvironmentQuery).getRequiredProperty$(key, defaultValue),
+    });
+  };
+}
+
+/**
+ * Gets the environment property at path as mutable and sets it in the property.
+ * @param key The property path to set.
+ * @param defaultValue The value to return if the path cannot be resolved.
+ * @throws If no `ServiceInjectorModule` imported.
+ */
+export function RequiredValue<D extends Property>(
+  key: Path,
+  defaultValue: D,
+): (target: unknown, propertyKey: string) => void {
+  return (target: unknown, propertyKey: string) => {
+    Object.defineProperty(target, propertyKey, {
+      value: serviceInjector(EnvironmentQuery).getRequiredProperty(key, defaultValue),
+    });
+  };
+}
--- packages/environment/angular/src/lib/decorators/value.decorator.ts
+++ packages/environment/angular/src/lib/decorators/value.decorator.ts
@@ -1,14 +1,13 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { serviceInjector } from '@kaikokeke/angular';
 import { EnvironmentQuery, Path } from '@kaikokeke/environment';
 
 /**
- * Sets the distinct environment property at path as mutable.
- * @param key The property path to set.
+ * Gets the distinct environment property at path as mutable and sets it in the property.
+ * @param key The property path to resolve.
  * @throws If no `ServiceInjectorModule` imported.
  */
-export function Value$(key: Path): (target: any, propertyKey: string) => void {
-  return (target: any, propertyKey: string) => {
+export function Value$(key: Path): (target: unknown, propertyKey: string) => void {
+  return (target: unknown, propertyKey: string) => {
     Object.defineProperty(target, propertyKey, {
       value: serviceInjector(EnvironmentQuery).getProperty$(key),
     });
@@ -16,12 +15,12 @@ export function Value$(key: Path): (target: any, propertyKey: string) => void {
 }
 
 /**
- * Sets the environment property at path as mutable.
- * @param key The property path to set.
+ * Gets the environment property at path as mutable and sets it in the property.
+ * @param key The property path to resolve.
  * @throws If no `ServiceInjectorModule` imported.
  */
-export function Value(key: Path): (target: any, propertyKey: string) => void {
-  return (target: any, propertyKey: string) => {
+export function Value(key: Path): (target: unknown, propertyKey: string) => void {
+  return (target: unknown, propertyKey: string) => {
     Object.defineProperty(target, propertyKey, {
       value: serviceInjector(EnvironmentQuery).getProperty(key),
     });
